e1b4c633a8cefa7d8ad97022911c4cfefa08e256
Guilherme Rossato <guilhermrossato@gmail.com>
2019-09-01 19:00:02 -0300

    Extended marvel fetcher test to fetch a story

--- tests/MarvelFetcherTest.php
+++ tests/MarvelFetcherTest.php
@@ -4,6 +4,35 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
 use Rossato\Marvel\Fetcher;
+use Rossato\Marvel\Model\Story;
+
+/**
+ * A Client mocked with a partial (but relevant) PSR7 interface.
+ */
+class MockedClient {
+	public function request($method, $url, $parameters) {
+		return new MockedResponse($method, $url, $parameters);
+	}
+}
+
+/**
+ * A Response class with a partial (but relevant) PSR7 interface.
+ */
+class MockedResponse {
+	public function getBody() {
+		return json_encode([
+			"data" => [
+				"results" => [
+					[
+						"id" => 1,
+						"title" => "Mocked Story",
+						"resourceURI" => "http://gateway.marvel.com/v1/public/stories/1"
+					]
+				]
+			]
+		]);
+	}
+}
 
 class MarvelFetcherTest extends TestCase {
 	public function testCreatesWithoutErrors() {
@@ -22,5 +51,16 @@ class MarvelFetcherTest extends TestCase {
 		$this->expectException(TypeError::class);
 
 		new Fetcher(1, 2);
+	}
+
+	public function testFetchesAStory() {
+		$fetcher = new Fetcher("publickey", "privatekey");
+		$fetcher->setClient(new MockedClient);
+
+		$storyId = getenv("MARVEL_STORY_ID");
+		$story = $fetcher->getStory($storyId);
+
+		$this->assertInstanceOf(Story::class, $story);
+		$this->assertEquals($story->name, "Mocked Story");
 	}
 }
